58e23be79a6ee2365543fd77ab99c6f2c857e5a6
Amber <arunge33@gmail.com>
2018-01-08 20:45:09 -0500

    final

--- assets/giphy.js
+++ assets/giphy.js
@@ -1,70 +1,69 @@
 
-var marvel = ["Hulk", "Ant Man", "Loki", "Shield"];
+var marvel = [];
 
-function alertmarvelName() {
-  var marvelName = $(this).attr("data-name");
-
-  alert(marvelName);
-}
-                    function renderButtons() {
-                      $("#buttons-view").empty();
-
-                    for (var i = 0; i < marvel.length; i++) {
-
-                      var button = $("<button>");
-
-                      button.addClass("marvel");
-
-                      button.attr("data-name", marvel[i]);
-
-                      button.text(marvel[i]);
-
-                      $("#buttons-view").append(button);
-  }
-}
-
-$("#add-marvel").on("click", function(event) {
-
-  event.preventDefault();
-
-  var character = $("#character-input").val().trim();
-
-  marvel.push(character);
-
-renderButtons();
-
-});
-
-$(document).on("click", ".character", alertmarvelName);
-
-renderButtons();
+function displayMarvelName() {}
 
 $("button").on("click", function() {
-  var person = $(this).attr("data-name");
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    person + "&api_key=dc6zaTOxFJmzC&limit=10";
 
+  var hero = $(this).data("search");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    hero + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+  // Performing our AJAX GET request
   $.ajax({
       url: queryURL,
       method: "GET"
     })
+
     .done(function(response) {
+
       var results = response.data;
-
       for (var i = 0; i < results.length; i++) {
-        var gifDiv = $("<div class='item'>");
 
-        var rating = results[i].rating;
+      var gifDiv = $("<div class='item'>");
+      var rating = results[i].rating;
+      var p = $("<p>").text("Rating: " + rating);
+      var heroImage = $("<img>");
 
-        var p = $("<p>").text("Rating: " + rating);
+      heroImage.attr("src", results[i].images.fixed_height.url);
+      gifDiv.append(p);
+      gifDiv.append(heroImage);
 
-        var personImage = $("<img>");
-        personImage.attr("src", results[i].images.fixed_height.url);
+    $("#gifSpace").prepend(gifDiv);
 
-        gifDiv.prepend(p);
-        gifDiv.prepend(personImage);
 
-        $("#gifs-go-here").prepend(gifDiv);
       }
     });
+
+
+function renderButtons(){
+
+  $("myButtons").empty();
+  for (var i = 0; i < marvel.length; i++) {
+    var a = $('<button class="btn btn-primary">');
+    a.addClass("comic");
+    a.attr("data-search", marvel[i]);
+    a.text(marvel[i]);
+    $("#myButtons").append(a);
+
+  }
+}
+
+$("addMarvel").on("click", function(event) {
+  event.preventDefault();
+  var guys = $("#characterInput").val().trim();
+  marvel.push(guys);
+
+
+  renderButtons();
+
+
+
 });
+
+
+$(document).on("click", ".comic", displayMarvelName);
+
+renderButtons();
+
+})
